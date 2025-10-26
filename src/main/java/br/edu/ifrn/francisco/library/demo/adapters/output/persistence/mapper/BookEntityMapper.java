76f75b5e86667462b981ff71473606b920df4f6d
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2025-10-26 10:00:46 -0300

    fix: adjusts mappers to avoid stack overflow

--- src/main/java/br/edu/ifrn/francisco/library/demo/adapters/output/persistence/mapper/BookEntityMapper.java
+++ src/main/java/br/edu/ifrn/francisco/library/demo/adapters/output/persistence/mapper/BookEntityMapper.java
@@ -3,9 +3,11 @@ package br.edu.ifrn.francisco.library.demo.adapters.output.persistence.mapper;
 import br.edu.ifrn.francisco.library.demo.adapters.output.persistence.entity.BookEntity;
 import br.edu.ifrn.francisco.library.demo.domain.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {AuthorEntityMapper.class, CategoryEntityMapper.class, LoanEntityMapper.class})
 public interface BookEntityMapper {
+    @Mapping(target = "bookLoans", ignore = true)
     Book toDomain(BookEntity entity);
 
     BookEntity toEntity(Book domain);
--- src/main/java/br/edu/ifrn/francisco/library/demo/adapters/output/persistence/mapper/LoanEntityMapper.java
+++ src/main/java/br/edu/ifrn/francisco/library/demo/adapters/output/persistence/mapper/LoanEntityMapper.java
@@ -3,9 +3,12 @@ package br.edu.ifrn.francisco.library.demo.adapters.output.persistence.mapper;
 import br.edu.ifrn.francisco.library.demo.adapters.output.persistence.entity.LoanEntity;
 import br.edu.ifrn.francisco.library.demo.domain.model.Loan;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {UserEntityMapper.class})
 public interface LoanEntityMapper {
+    @Mapping(target = "items", ignore = true)
     Loan toDomain(LoanEntity entity);
+
     LoanEntity toEntity(Loan domain);
 }
